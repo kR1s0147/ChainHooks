f23d9bace45d400639c2465301a3229e52615757
kR1s0147 <giridharchukkala@gmail.com>
2025-07-23 18:29:42 +0530

    rawtransactions

--- src/relayer/transactionTypes.rs
+++ src/relayer/transactionTypes.rs
@@ -1,21 +1,12 @@
-use alloy::json_abi::JsonAbi;
-use alloy::primitives::{Address, address};
+use alloy::dyn_abi::{DynSolType, DynSolValue, JsonAbiExt};
+use alloy::json_abi::{Function, JsonAbi, Param};
+use alloy::network::TransactionBuilder;
+use alloy::primitives::{Address, Bytes, FixedBytes, address};
+use alloy::rpc::types::Log;
 use alloy::rpc::types::TransactionRequest;
-use alloy::rpc_types::Log;
-use serde::de::Error;
 use serde_json::Value;
+use serde_json::json;
 use thiserror::Error;
-
-pub struct TransactionBuilder {
-    contract_address: Address,
-    abi: JsonAbi,
-    function_name: String,
-    params: Vec<Value>,
-}
-
-impl TransactionBuilder {
-    fn build(self, log: Log) -> Result<TransactionRequest, Box<dyn Error>> {}
-}
 
 #[derive(Error, Debug)]
 pub enum RelayerError {
@@ -25,40 +16,107 @@ pub enum RelayerError {
     ChainHasNoRpcURL,
     #[error("User subscription is not found")]
     NoSubscriptionFound,
+    #[error("invalid transaction")]
+    InvalidTransactionRequest,
+    #[error("Function not found")]
+    FunctionNotFound,
+    #[error("Invalid Data Type")]
+    InvalidDataType,
+    #[error("Invalid topic mapping")]
+    InvalidTopicMapping,
+    #[error("Invalid args count")]
+    InvalidArgsCount,
+    #[error("Topic Range Out of Index")]
+    TopicOutOfIndex,
 }
 
 pub struct RawTransaction {
     contract_address: String,
     abi: String,
     function_name: String,
+    function_signature: String,
     params: Vec<(usize, String)>,
 }
 
 impl RawTransaction {
-    fn build_transaction(self) -> Result<TransactionBuilder, Box<dyn Error>> {
-        let contract_addr = Address::from_str(self.contract_address.as_str());
+    fn build_transaction(self, log: Log) -> Result<TransactionRequest, Box<dyn std::error::Error>> {
+        let contract_addr = Address::from_slice(self.contract_address.as_bytes());
         let ABI: JsonAbi;
+
         match serde_json::from_str(&self.abi) {
             Ok(abi) => ABI = abi,
             Err(e) => return Err(Box::new(RelayerError::InvalidABI)),
         }
-        let params = Vec::new();
-        for (index, param) in self.params {
-            if param.contains("topic") {
-                let n = format!("topic{index}");
-                params.push(n);
+
+        let function_val = ABI.function(&self.function_name).unwrap().first();
+        let function: Function;
+
+        match function_val {
+            Some(func) => {
+                function = func.clone();
+            }
+            None => return Err(Box::new(RelayerError::FunctionNotFound)),
+        }
+
+        let topics = log.topics();
+        let mut resolved_params: Vec<String> = Vec::new();
+
+        for source in &self.params {
+            let param_str = &source.1;
+
+            if param_str.starts_with("topic") {
+                let index_str = &param_str[5..];
+                let index: usize = index_str.parse().unwrap();
+
+                let topic_value = topics.get(index);
+                let topic: &FixedBytes<32>;
+                match topic_value {
+                    Some(t) => topic = t,
+                    None => return Err(Box::new(RelayerError::InvalidTopicMapping)),
+                }
+
+                resolved_params.push(topic.to_string());
             } else {
-                params.push(param);
+                resolved_params.push(param_str.clone());
+            }
+        }
+        let sol_values = self
+            .convert_strings_to_sol_values(&resolved_params, &function.inputs)
+            .unwrap();
+        let data = function.abi_encode_input(&sol_values).unwrap();
+
+        let transaction = TransactionRequest::default()
+            .to(contract_addr)
+            .with_input(Bytes::from(data));
+
+        Ok(transaction)
+    }
+
+    fn convert_strings_to_sol_values<'a>(
+        &self,
+        params: &[String],
+        abi_inputs: &'a [Param],
+    ) -> Result<Vec<DynSolValue>, Box<dyn std::error::Error>> {
+        if params.len() != abi_inputs.len() {
+            return Err(Box::new(RelayerError::InvalidArgsCount));
+        }
+        let param_sols = params.iter().zip(abi_inputs.iter());
+        let mut solval: Vec<DynSolValue> = Vec::new();
+        for (param, abi_in) in param_sols {
+            let sol_type: DynSolType = match abi_in.ty.parse() {
+                Ok(solv) => solv,
+                Err(_) => return Err(Box::new(RelayerError::InvalidDataType)),
+            };
+
+            let solvalue = sol_type.coerce_str(&param);
+            match solvalue {
+                Ok(sol) => {
+                    solval.push(sol);
+                }
+                Err(_) => return Err(Box::new(RelayerError::InvalidDataType)),
             }
         }
 
-        let tran = TransactionBuilder {
-            contract_address: contract_addr,
-            abi: ABI,
-            function_name: self.function_name,
-            params: params,
-        };
-
-        return Ok(tran);
+        Ok(solval)
     }
 }
